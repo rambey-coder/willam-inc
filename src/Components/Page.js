4233bc1f1983549f012934527eb3a3ce6705f2be
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-17 12:26:06 +0100

    navbar done

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import './Page.css'
+import { Link } from 'react-router-dom'
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <div className="page-container">
+    <div className="nav-sec">
+      <nav className="nav">
+        <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo'/>
+
+        <div className="lists">
+          <Link to='#' className='link'>Products</Link>
+          <Link to='#' className='link'>Pricing</Link>
+          <Link to='#' className='link'>Blog</Link>
+          <Link to='#' className='link'>Contact</Link>
+          <Link to='#' className='link'>FAQs</Link>
+          <span>
+            <Link to='#' className='sign-in'>Sign In</Link>
+          </span>
+        </div>
+      </nav>
+
+      <div className="hero-section">
+        <h1>Collaborate. Track workflow. Manage the Team</h1>
+      </div>
+      </div>
+    </div>
   )
 }
 
