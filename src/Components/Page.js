3d5a7949924454f0a18a1ab9debb3aa7f281594e
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-18 02:47:52 +0100

    brand secction

--- src/Components/Page.js
+++ src/Components/Page.js
@@ -5,43 +5,60 @@ import { Link } from 'react-router-dom'
 const Page = () => {
   return (
     <div className="page-container">
-    <div className="nav-sec">
-      <nav className="nav">
-        <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo'/>
+      <div className="nav-sec">
+        <nav className="nav">
+          <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo' />
 
-        <div className="lists">
-          <Link to='#' className='link'>Products</Link>
-          <Link to='#' className='link'>Pricing</Link>
-          <Link to='#' className='link'>Blog</Link>
-          <Link to='#' className='link'>Contact</Link>
-          <Link to='#' className='link'>FAQs</Link>
-          <span>
-            <Link to='/' className='sign-in'>Sign Up</Link>
-          </span>
-        </div>
-      </nav>
+          <div className="lists">
+            <Link to='#' className='link'>Products</Link>
+            <Link to='#' className='link'>Pricing</Link>
+            <Link to='#' className='link'>Blog</Link>
+            <Link to='#' className='link'>Contact</Link>
+            <Link to='#' className='link'>FAQs</Link>
+            <span>
+              <Link to='/' className='sign-in'>Sign Up</Link>
+            </span>
+          </div>
+        </nav>
 
-      <div className="hero-section">
-        <h1>Collaborate. Track workflow. <br /> Manage the Team</h1>
-        <p>Try workflo today and increase your companys productivity, effectiveness and attain great turn over with more happy clients</p>
-        <button>Try it for free</button>
-        <div className="figure">
-          <span>
-            <p>15,000+</p>
-            <p>Users</p>
-          </span>
+        <div className="hero-section">
+          <h1>Collaborate. Track workflow. <br /> Manage the Team</h1>
+          <p>Try workflo today and increase your companys productivity, effectiveness and attain great turn over with more happy clients</p>
+          <button>Try it for free</button>
+          <div className="figure">
+            <span>
+              <p>15,000+</p>
+              <p>Users</p>
+            </span>
 
-          <span>
-            <p>400+</p>
-            <p>Companies</p>
-          </span>
+            <span>
+              <p>400+</p>
+              <p>Companies</p>
+            </span>
 
-          <span>
-            <p>2 times</p>
-            <p>Workspace of the year</p>
-          </span>
+            <span>
+              <p>2 times</p>
+              <p>Workspace of the year</p>
+            </span>
+          </div>
         </div>
       </div>
+
+      <div className="dashboard">
+          <img src="./Assets/Dashboard.svg" alt="dashboard" className='dashboard-img'/>
+        </div>
+
+      <div className="main-section">
+        <div className="brand-section">
+          <h3>Worlds Top brands trust us with their projects</h3>
+          <div className="img-container">
+            <img src="./Assets/google-logo.svg" alt="google" />
+            <img src="./Assets/flutterwave-logo.svg" alt="flutterwave" />
+            <img src="./Assets/meta-logo.svg" alt="meta" />
+            <img src="./Assets/paystack-logo.svg" alt="paystack" />
+            <img src="./Assets/Dangote-logo.svg" alt="Dangote" />
+          </div>
+        </div>
       </div>
     </div>
   )
