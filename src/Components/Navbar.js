b54549b90ad8562ba904158f343cdd3bd8b9da58
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-18 16:09:09 +0100

    hamburger menu added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,24 +1,33 @@
 import React from 'react'
 import './NavFoot.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
-  return (
-        <nav className="nav">
-          <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo' />
+    const [click, setClick] = useState(false)
+    
+    const handleClick = () => setClick(!click)
 
-          <div className="lists">
-            <Link to='#' className='link'>Products</Link>
-            <Link to='#' className='link'>Pricing</Link>
-            <Link to='#' className='link'>Blog</Link>
-            <Link to='#' className='link'>Contact</Link>
-            <Link to='#' className='link'>FAQs</Link>
-            <span>
-              <Link to='/' className='sign-in'>Sign Up</Link>
-            </span>
-          </div>
+    return (
+        <nav className="nav">
+            <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo' />
+
+            <div className="lists">
+                <Link to='#' className='link'>Products</Link>
+                <Link to='#' className='link'>Pricing</Link>
+                <Link to='#' className='link'>Blog</Link>
+                <Link to='#' className='link'>Contact</Link>
+                <Link to='#' className='link'>FAQs</Link>
+                <span>
+                    <Link to='/' className='sign-in'>Sign Up</Link>
+                </span>
+            </div>
+
+            <div className="mobile-icon" onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
         </nav>
-  )
+    )
 }
 
 export default Navbar
