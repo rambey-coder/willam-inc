ad0cac1f51870dd27ca82d48b3aeacddba750d62
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-18 16:08:15 +0100

    all done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,32 +3,40 @@ import './NavFoot.css'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  return (
-    <div className="footer-section">
-        <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo' />
+    return (
+        <footer className="footer-section">
+            <div className="nav-foot">
+                <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo' />
 
-        <div className="lists">
-            <Link to='#' className='link'>Products</Link>
-            <Link to='#' className='link'>Pricing</Link>
-            <Link to='#' className='link'>Blog</Link>
-            <Link to='#' className='link'>Contact</Link>
-            <Link to='#' className='link'>FAQs</Link>
-            <Link to='#' className='link'>Terms & Conditions</Link>
-            <Link to='#' className='link'>Privacy Policy</Link>
-          </div>
+                <div className="lists">
+                    <Link to='#' className='link'>Products</Link>
+                    <Link to='#' className='link'>Pricing</Link>
+                    <Link to='#' className='link'>Blog</Link>
+                    <Link to='#' className='link'>Contact</Link>
+                    <Link to='#' className='link'>FAQs</Link>
+                    <Link to='#' className='link'>Terms & Conditions</Link>
+                    <Link to='#' className='link'>Privacy Policy</Link>
+                </div>
+            </div>
 
-          <hr />
+            <hr />
 
-          <div className="foot">
-            <p>&#169; 2022 Workflo Org. All rights reserved</p>
-            <span>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-brands fa-twitter"></i>
-            <i class="fa-brands fa-instagram"></i>
-            </span>
-          </div>
-    </div>
-  )
+            <div className="foot">
+                <p>&#169; 2022 Workflo Org. All rights reserved</p>
+                <span>
+                    <a href="">
+                        <i class="fa-brands fa-linkedin"></i>
+                    </a>
+                    <a href="">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href="">
+                        <i class="fa-brands fa-instagram"></i>
+                    </a>
+                </span>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
