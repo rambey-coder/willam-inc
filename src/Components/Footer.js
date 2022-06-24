0724cdc9b495daab01e22abe21b40133b6f52367
Rambey <olayiwolaagboolaakanji@gmail.com>
2022-06-24 10:53:35 +0100

    social links added

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,10 +9,10 @@ const Footer = () => {
                 <img src="./Assets/Workflo-logo.svg" alt="logo" className='page-logo' />
 
                 <div className="lists-footer">
-                    <Link to='#' className='link'>Products</Link>
+                    <Link to='/page' className='link'>Products</Link>
                     <Link to='#' className='link'>Pricing</Link>
                     <Link to='#' className='link'>Blog</Link>
-                    <Link to='#' className='link'>Contact</Link>
+                    <Link to='/contact' className='link'>Contact</Link>
                     <Link to='#' className='link'>FAQs</Link>
                     <Link to='#' className='link'>Terms & Conditions</Link>
                     <Link to='#' className='link'>Privacy Policy</Link>
@@ -24,13 +24,13 @@ const Footer = () => {
             <div className="foot">
                 <p>&#169; 2022 Workflo Org. All rights reserved</p>
                 <span>
-                    <a href="">
+                    <a href="https://www.linkedin.com/">
                         <i className="fa-brands fa-linkedin"></i>
                     </a>
-                    <a href="">
+                    <a href="https://twitter.com/">
                         <i className="fa-brands fa-twitter"></i>
                     </a>
-                    <a href="">
+                    <a href="https://www.instagram.com/">
                         <i className="fa-brands fa-instagram"></i>
                     </a>
                 </span>
